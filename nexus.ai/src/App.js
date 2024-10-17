e29af9a6255b7ce15a07f057fe95a7026f0b7023
Lucas Candido <augusthocandido3@outlook.com>
2024-10-17 16:51:48 -0300

    organiza elemntos de interface
    Em processo de teste para a funcionalidade de ao clicar no botão consultar, abrir uma nova tela com a resposta do GPT

--- nexus.ai/src/App.js
+++ nexus.ai/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Historico from './components/Historico';
 import NovaConsulta from './components/NovaConsulta';
 import Sugestoes from './components/Sugestoes'
+import TelaPesquisa from './components/TelaPesquisa'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,7 +11,14 @@ function App() {
       <Historico />
       <NovaConsulta />
       <Sugestoes />
+    {/* <Router>
+      <Routes>
+        <Route path='/' element={<botao-consultar />} />
+        <Route path='/resposta-gpt' element={<TelaPesquisa />} />
+      </Routes>
+    </Router> */}
     </div>
+
   );
 }
 
