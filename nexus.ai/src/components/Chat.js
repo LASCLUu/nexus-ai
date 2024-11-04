6f693e96562dda6f7b25fe5c04f70c7d2807af8d
gustavo-pupo <gusfepupo@gmail.com>
2024-11-04 20:10:49 -0300

    adiciona lógica de troca de renderização de conteúdos

---
+++ nexus.ai/src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chat = () => {
+    return(
+        <div className="chat">
+            <textarea></textarea>
+            <button>Enviar</button>
+        </div>
+    )
+}
+
+export default Chat;
--- nexus.ai/src/pages/main/index.js
+++ nexus.ai/src/pages/main/index.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import "./main.css";
 import Historico from "../../components/Historico";
 import NovaConsulta from "../../components/NovaConsulta";
 import Sugestoes from "../../components/Sugestoes";
 import logo from "../../assets/logo-nexus.png";
+import Chat from "../../components/Chat";
 
 const Main = () => {
-  return (
-    <>
-      <header id="header">
-        <img src={logo} alt="Logo" id="logo" />
-        <h1 id="titulo">Seu assistente acadêmico inteligente!</h1>
-      </header>
-      <div className="App">
+  const [conteudo, setConteudo] = useState("current")
+
+  const renderComponent = () => {
+    if(conteudo === true){
+      return <Chat />
+    } else {
+      return <>
         <Historico />
         <NovaConsulta />
         <Sugestoes />
+      </>
+    }
+  }
+
+  return (
+    <>
+      <header id="header">
+        <img src={logo} alt="Logo" id="logo" onClick={() => setConteudo(c => !c)}/>
+        <h1 id="titulo">Seu assistente acadêmico inteligente!</h1>
+      </header>
+      <div className="App">
+        {renderComponent()}
       </div>
       <footer id="footer">
         <p id="nexus">
